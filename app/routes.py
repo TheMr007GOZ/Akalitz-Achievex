4b2d50efb8b069db9395fbc7ceeff42583a421b2
jevisan <jorgevigilsantos@gmail.com>
2020-05-02 17:40:59 -0500

    Base app template

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Jevisan'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')

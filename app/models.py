0daead7498faf5910fcac4f8d0e4a33cf24aaee2
jevisan <jorgevigilsantos@gmail.com>
2020-05-21 17:31:31 -0500

    Funcionalidad basica logros y medallas

--- app/models.py
+++ app/models.py
@@ -7,18 +7,27 @@ from flask_login import UserMixin
 def load_user(id):
     return User.query.get(int(id))
 
+medallas_users = db.Table('medallas_users',
+                            db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False),
+                            db.Column('medalla_id', db.Integer, db.ForeignKey('medalla.id'), nullable=False),
+                            db.PrimaryKeyConstraint('user_id', 'medalla_id'))
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    logros = db.relationship('Logro', backref='usuario', lazy='dynamic')
+    logros = db.relationship('Logro', backref='user', lazy='dynamic')
+    medallas = db.relationship('Medalla', secondary=medallas_users, backref='user')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def medallas_count(self):
+        return db.session.query(Medalla).with_parent(self, "medallas").count()
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -29,7 +38,17 @@ class Logro(db.Model):
     nombre = db.Column(db.String(64))
     descripcion = db.Column(db.String(240))
     casi_logro = db.Column(db.Boolean())
+    logrado = db.Column(db.Boolean())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Logro {}>'.format(self.nombre)
+
+
+class Medalla(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(64))
+    descripcion = db.Column(db.String(240))
+
+    def __repr__(self):
+        return '<Medalla {}>'.format(self.nombre)
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,28 @@ from app import app, db
 from flask_login import login_required
 from flask_login import current_user, login_user, logout_user
 from app.forms import LoginForm, RegistrationForm, LogroForm
-from app.models import User, Logro
+from app.models import User, Logro, Medalla
+
+
+def add_medalla(medalla):
+    current_user.medallas.append(medalla)
+    db.session.commit()
+    flash('Nuevo medalla obtenida: {}'.format(medalla.nombre))
+
+def check_medallas_to_add():
+    cuenta_logros = current_user.logros.count()
+    if cuenta_logros == 1:
+        print("una medalla")
+        add_medalla(Medalla.query.filter_by(nombre="Primer paso").first())
+    if cuenta_logros == 10:
+        print("diez medallas")
+        add_medalla(Medalla.query.filter_by(nombre="Diez seguidos").first())
+    if cuenta_logros == 25:
+        print("25 medallas")
+        add_medalla(Medalla.query.filter_by(nombre="5 x 5").first())
+    if cuenta_logros == 50:
+        print("50 medallas")
+        add_medalla(Medalla.query.filter_by(nombre="L").first())
 
 
 @app.route('/')
@@ -17,7 +38,10 @@ def index():
 @app.route('/home')
 @login_required
 def home():
-    return render_template('home.html', title="Home")
+    logros = current_user.logros
+    # medallas = db.session.query(Medalla).with_parent(current_user, "medallas").count() # alternative query
+    medallas = current_user.medallas_count()
+    return render_template('home.html', title="Home", logros=logros, medallascount=medallas)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -62,9 +86,24 @@ def logout():
 def nuevo_logro():
     form = LogroForm()
     if form.validate_on_submit():
-        logro = Logro(nombre=form.nombre.data, descripcion=form.descripcion.data)
+        logro = Logro(nombre=form.nombre.data, descripcion=form.descripcion.data, user=current_user)
         db.session.add(logro)
         db.session.commit()
         flash('Nuevo logro creado: {}'.format(logro.nombre))
+        check_medallas_to_add()
         return redirect(url_for('home'))
     return render_template('nuevo_logro.html', title='Nuevo logro', form=form)
+
+
+@app.route('/logros')
+@login_required
+def logros():
+    logros = current_user.logros
+    return render_template('logros.html', title='Lista de logros', logros=logros)
+
+
+@app.route('/medallas')
+def medallas():
+    medallas_disponibles = Medalla.query.all()
+    medallas_conseguidas = current_user.medallas
+    return render_template('medallas.html', title='Lista de medallas', medallas=medallas_conseguidas, medallas_disponibles=medallas_disponibles)
